11291aa52e664215105dc17ec9864817835ffc4f
Laís <laisandriellyz@gmail.com>
2026-09-21 06:47:38 -0300

    Implementadas as operações READ, UPDATE e DELETE da API REST

--- src/main/java/br/senac/tads/dsw/exemplo2/controller/ProdutoController.java
+++ src/main/java/br/senac/tads/dsw/exemplo2/controller/ProdutoController.java
@@ -1,9 +1,14 @@
 package br.senac.tads.dsw.exemplo2.controller;
-
 import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,4 +41,53 @@ public class ProdutoController {
         return ResponseEntity.created(location).body(produtoSalvo);
     }
 
+    // Retorna a lista completa de produtos armazenados no banco de dados.
+@GetMapping
+public List<Produto> listarTodos() {
+    // O método findAll() do repositório gera automaticamente um SELECT * FROM produto.
+    return repository.findAll();
+}
+
+// A notação {id} representa um parâmetro de caminho (PathVariable).
+// A URL esperada é /api/produtos/1
+@GetMapping("/{id}")
+public ResponseEntity<Produto> buscarPorId(@PathVariable Long id) {
+    // O método findById retorna um Optional.
+    Optional<Produto> produtoBuscado = repository.findById(id);
+
+    if (produtoBuscado.isPresent()) {
+        return ResponseEntity.ok(produtoBuscado.get());
+    } else {
+        return ResponseEntity.notFound().build();
+    }
+}
+@PutMapping("/{id}")
+public ResponseEntity<Produto> atualizarProduto(@PathVariable Long id, @RequestBody Produto produtoAtualizado) {
+
+    Optional<Produto> produtoBuscado = repository.findById(id);
+
+    if (produtoBuscado.isPresent()) {
+        Produto produtoExistente = produtoBuscado.get();
+
+        produtoExistente.setNome(produtoAtualizado.getNome());
+        produtoExistente.setPreco(produtoAtualizado.getPreco());
+
+        Produto produtoSalvo = repository.save(produtoExistente);
+
+        return ResponseEntity.ok(produtoSalvo);
+    } else {
+        return ResponseEntity.notFound().build();
+    }
+}
+@DeleteMapping("/{id}")
+public ResponseEntity<Void> apagarProduto(@PathVariable Long id) {
+    Optional<Produto> produtoBuscado = repository.findById(id);
+
+    if (produtoBuscado.isPresent()) {
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    } else {
+        return ResponseEntity.notFound().build();
+    }
+}
 }
